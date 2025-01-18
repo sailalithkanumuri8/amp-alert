62b537f4e14c0a50be6c866b000931506f9a3257
Rohan Nair <rohanmanunair@gmail.com>
2025-01-18 14:26:28 -0500

    Dummy camera

--- nodeAmpAlert/app/(tabs)/explore.tsx
+++ nodeAmpAlert/app/(tabs)/explore.tsx
@@ -98,7 +98,7 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   headerImage: {
     color: '#808080',
-    bottom: -90,
+    bottom: -50,
     left: -35,
     position: 'absolute',
   },
